265807edf331025c1bc156619027070699d1d1ed
AndrezAguayo <andrez@aguayo.me>
2015-05-04 12:16:23 -0500

    Small change
    Changed the layout

--- Python/gitHubTest.py
+++ Python/gitHubTest.py
@@ -1,6 +1,6 @@
 ## this is a test of git hub ## 
-## Dr.Dre and Tim Little will be rocking the house with 
-## this gangsa tec 
+## Dr.Dre and Tim Little will be rocking the 
+## house with this gangsta tec 
 
 def an_extream_function():
 	print "an apple pie a day keeps the doctor away"
